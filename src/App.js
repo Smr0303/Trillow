319f0bad3cec71fccfbb20bb7426f14f7c3b93e7
Smr0303 <samarmohite7@gmail.com>
2023-01-17 19:14:27 +0530

    Nft data fetched form ipfs

--- src/App.js
+++ src/App.js
@@ -15,19 +15,45 @@ import config from './config.json';
 
 function App() {
   const [account,setAccount] = useState(null);
-
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [homes,setHomes]= useState([]);
+  
   const loadBlockchainData = async()=>{
     if(typeof window.ethereum === "undefined"){
       alert("Please install metamask");
     }
     else{
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-       window.ethereum.on("accountsChanged", async() =>{
+      setProvider(provider);
+      //On account change automatically update the account
+      const network = await provider.getNetwork();
+      
+      const realEstate = await new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider);
+      
+      const totalSupply = await realEstate.totalSupply();
+      
+      const homesArray=[];
+      
+      for(var i=0; i<totalSupply; i++){
+        const uri = await realEstate.tokenURI(i+1);
+        const res = await fetch(uri);
+        const metadata = await res.json();
+        homesArray.push(metadata);
+      }
+      await setHomes(homesArray);
+
+      const escrow = await new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider);
+      setEscrow(escrow);
+
+      window.ethereum.on("accountsChanged", async() =>{
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = ethers.utils.getAddress(accounts[0])
         setAccount(account); 
-       }) 
-    
+        console.log(account);
+        
+      }) 
+      
     }
 
 
@@ -39,12 +65,38 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}/>
+      <Search/>
 
       <div className='cards__section'>
 
-        <h3>Welcome to Trillow</h3>
+        <h3>Homes For You</h3>
+
+        <hr />
+
+        <div className='cards'>
+          {homes.map((home, index) => (
+            <div className='card' key={index} >
+              <div className='card__image'>
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+          ))}
+        </div>
 
       </div>
+
+      {/* {toggle && (
+        <Home home={home} provider={provider} account={account} escrow={escrow} togglePop={togglePop} />
+      )} */}
 
     </div>
   );
